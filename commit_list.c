041d93814efd1f32c65a2ba32f423272cea50f85
Balázs János Varga <vargab95@gmail.com>
2021-10-24 17:51:14 +0200

    Reformat commit_list.c

--- commit_list.c
+++ commit_list.c
@@ -23,8 +23,7 @@ void commit_list_destroy(commit_list_t **commit_list)
     uint32_t tmp_str_len;
 
     commit_iterator = m_list_iterator_create((*commit_list)->commit_list);
-    for (m_list_iterator_go_to_head(commit_iterator);
-         (tmp = m_list_iterator_current(commit_iterator)) != NULL;
+    for (m_list_iterator_go_to_head(commit_iterator); (tmp = m_list_iterator_current(commit_iterator)) != NULL;
          m_list_iterator_next(commit_iterator))
     {
         commit_destroy((commit_t **)&tmp->data);
@@ -53,8 +52,7 @@ void commit_destroy(commit_t **commit)
     m_list_iterator_t *change_iterator;
 
     change_iterator = m_list_iterator_create((*commit)->change_list);
-    for (m_list_iterator_go_to_head(change_iterator);
-         (tmp = m_list_iterator_current(change_iterator)) != NULL;
+    for (m_list_iterator_go_to_head(change_iterator); (tmp = m_list_iterator_current(change_iterator)) != NULL;
          m_list_iterator_next(change_iterator))
     {
         change_t *change = tmp->data;
@@ -81,11 +79,9 @@ void commit_list_dump(const commit_list_t *const commit_list, FILE *output_file)
     tmp_str_len = strlen(commit_list->last_commit_id);
     fwrite(&tmp_str_len, sizeof(tmp_str_len), 1, output_file);
     fwrite(commit_list->last_commit_id, sizeof(char), tmp_str_len, output_file);
-    fwrite(&commit_list->commit_list->size,
-           sizeof(commit_list->commit_list->size), 1, output_file);
+    fwrite(&commit_list->commit_list->size, sizeof(commit_list->commit_list->size), 1, output_file);
     commit_iterator = m_list_iterator_create(commit_list->commit_list);
-    for (m_list_iterator_go_to_head(commit_iterator);
-         (tmp = m_list_iterator_current(commit_iterator)) != NULL;
+    for (m_list_iterator_go_to_head(commit_iterator); (tmp = m_list_iterator_current(commit_iterator)) != NULL;
          m_list_iterator_next(commit_iterator))
     {
         commit_t *commit = tmp->data;
@@ -95,11 +91,9 @@ void commit_list_dump(const commit_list_t *const commit_list, FILE *output_file)
         fwrite(commit->commit_id, sizeof(char), tmp_str_len, output_file);
         fwrite(&commit->timestamp, sizeof(commit_timestamp_t), 1, output_file);
 
-        fwrite(&commit->change_list->size, sizeof(commit->change_list->size), 1,
-               output_file);
+        fwrite(&commit->change_list->size, sizeof(commit->change_list->size), 1, output_file);
         change_iterator = m_list_iterator_create(commit->change_list);
-        for (m_list_iterator_go_to_head(change_iterator);
-             (tmp = m_list_iterator_current(change_iterator)) != NULL;
+        for (m_list_iterator_go_to_head(change_iterator); (tmp = m_list_iterator_current(change_iterator)) != NULL;
              m_list_iterator_next(change_iterator))
         {
             change_t *change = tmp->data;
@@ -113,10 +107,8 @@ void commit_list_dump(const commit_list_t *const commit_list, FILE *output_file)
             fwrite(change->new_file, sizeof(char), tmp_str_len, output_file);
 
             fwrite(&change->type, sizeof(change->type), 1, output_file);
-            fwrite(&change->added_lines, sizeof(change->added_lines), 1,
-                   output_file);
-            fwrite(&change->deleted_lines, sizeof(change->deleted_lines), 1,
-                   output_file);
+            fwrite(&change->added_lines, sizeof(change->added_lines), 1, output_file);
+            fwrite(&change->deleted_lines, sizeof(change->deleted_lines), 1, output_file);
         }
         m_list_iterator_destroy(&change_iterator);
     }
@@ -129,8 +121,7 @@ void commit_list_load(commit_list_t *commit_list, FILE *input_file)
     uint32_t tmp_str_len, commit_list_size, change_list_size;
 
     fread(&tmp_str_len, sizeof(tmp_str_len), 1, input_file);
-    commit_list->last_commit_id =
-        (char *)m_mem_calloc(tmp_str_len + 1, sizeof(char));
+    commit_list->last_commit_id = (char *)m_mem_calloc(tmp_str_len + 1, sizeof(char));
     fread(commit_list->last_commit_id, sizeof(char), tmp_str_len, input_file);
     fread(&commit_list_size, sizeof(commit_list_size), 1, input_file);
     for (uint32_t i = 0; i < commit_list_size; i++)
@@ -149,20 +140,16 @@ void commit_list_load(commit_list_t *commit_list, FILE *input_file)
             change->commit = commit;
 
             fread(&tmp_str_len, sizeof(tmp_str_len), 1, input_file);
-            change->old_file =
-                (char *)m_mem_calloc(tmp_str_len + 1, sizeof(char));
+            change->old_file = (char *)m_mem_calloc(tmp_str_len + 1, sizeof(char));
             fread(change->old_file, sizeof(char), tmp_str_len, input_file);
 
             fread(&tmp_str_len, sizeof(tmp_str_len), 1, input_file);
-            change->new_file =
-                (char *)m_mem_calloc(tmp_str_len + 1, sizeof(char));
+            change->new_file = (char *)m_mem_calloc(tmp_str_len + 1, sizeof(char));
             fread(change->new_file, sizeof(char), tmp_str_len, input_file);
 
             fread(&change->type, sizeof(change->type), 1, input_file);
-            fread(&change->added_lines, sizeof(change->added_lines), 1,
-                  input_file);
-            fread(&change->deleted_lines, sizeof(change->deleted_lines), 1,
-                  input_file);
+            fread(&change->added_lines, sizeof(change->added_lines), 1, input_file);
+            fread(&change->deleted_lines, sizeof(change->deleted_lines), 1, input_file);
 
             tmp.size = sizeof(change);
             tmp.data = change;
@@ -184,8 +171,7 @@ void commit_list_print(const commit_list_t *const commit_list)
     puts("Commit list");
 
     commit_iterator = m_list_iterator_create(commit_list->commit_list);
-    for (m_list_iterator_go_to_head(commit_iterator);
-         (tmp = m_list_iterator_current(commit_iterator)) != NULL;
+    for (m_list_iterator_go_to_head(commit_iterator); (tmp = m_list_iterator_current(commit_iterator)) != NULL;
          m_list_iterator_next(commit_iterator))
     {
         char buffer[32];
@@ -195,8 +181,7 @@ void commit_list_print(const commit_list_t *const commit_list)
         printf("  %s %s\n", buffer, commit->commit_id);
 
         change_iterator = m_list_iterator_create(commit->change_list);
-        for (m_list_iterator_go_to_head(change_iterator);
-             (tmp = m_list_iterator_current(change_iterator)) != NULL;
+        for (m_list_iterator_go_to_head(change_iterator); (tmp = m_list_iterator_current(change_iterator)) != NULL;
              m_list_iterator_next(change_iterator))
         {
             change_t *change = tmp->data;
@@ -223,8 +208,7 @@ void commit_list_print(const commit_list_t *const commit_list)
 
             printf("    New file: %s, old file: %s, type: %s, added: %d, "
                    "deleted: %d\n",
-                   change->new_file, change->old_file, type,
-                   change->added_lines, change->deleted_lines);
+                   change->new_file, change->old_file, type, change->added_lines, change->deleted_lines);
         }
         m_list_iterator_destroy(&change_iterator);
     }
@@ -243,7 +227,7 @@ int each_file_cb(const git_diff_delta *delta, float progress, void *payload)
     tmp.data = change;
 
     m_list_append_to_end_set(commit->change_list, &tmp);
-    printf("%s %f\n", commit->commit_id, progress);
+    // printf("%s %f\n", commit->commit_id, progress);
 
     // puts("EACH FILE CB");
     switch (delta->status)
@@ -276,35 +260,34 @@ int each_file_cb(const git_diff_delta *delta, float progress, void *payload)
     change->old_file = strdup(delta->old_file.path);
     change->new_file = strdup(delta->new_file.path);
 
-    // printf("%s %d %d\n", commit->commit_id, delta->status,
-    // delta->similarity);
+    // printf("%s %d %d\n", commit->commit_id, delta->status, delta->similarity);
+    // printf("CID: %s OF: %s NF: %s T: %d\n", commit->commit_id, change->new_file, change->old_file, change->type);
     return 0;
 }
 
-int each_binary_cb(const git_diff_delta *delta, const git_diff_binary *binary,
-                   void *payload)
+int each_binary_cb(const git_diff_delta *delta, const git_diff_binary *binary, void *payload)
 {
     // puts("EACH BINARY CB");
     return 0;
 }
 
-int each_hunk_cb(const git_diff_delta *delta, const git_diff_hunk *hunk,
-                 void *payload)
+int each_hunk_cb(const git_diff_delta *delta, const git_diff_hunk *hunk, void *payload)
 {
     commit_t *commit = (commit_t *)payload;
     change_t *change = commit->change_list->tail->data.data;
     // // puts("EACH HUNK CB");
-
     change->added_lines += hunk->new_lines;
     change->deleted_lines += hunk->old_lines;
+
+    // printf("HUNK %s %s %d %d %d %d\n", change->new_file, change->old_file, hunk->new_lines, hunk->old_lines,
+    //       change->added_lines, change->deleted_lines);
 
     // printf("%s || - %d  + %d\n", commit->commit_id, hunk->old_lines,
     //        hunk->new_lines);
     return 0;
 }
 
-int each_line_cb(const git_diff_delta *delta, const git_diff_hunk *hunk,
-                 const git_diff_line *line, void *payload)
+int each_line_cb(const git_diff_delta *delta, const git_diff_hunk *hunk, const git_diff_line *line, void *payload)
 {
     // puts("EACH LINE CB");
     return 0;
@@ -345,10 +328,8 @@ void commit_list_fetch_local(commit_list_t *commit_list, const char *path)
     }
 
     old_last_commit_id = commit_list->last_commit_id;
-    commit_list->last_commit_id =
-        strdup(git_oid_tostr_s(git_commit_id(git_commit)));
-    if (old_last_commit_id &&
-        strcmp(old_last_commit_id, commit_list->last_commit_id) == 0)
+    commit_list->last_commit_id = strdup(git_oid_tostr_s(git_commit_id(git_commit)));
+    if (old_last_commit_id && strcmp(old_last_commit_id, commit_list->last_commit_id) == 0)
     {
         puts("Repo is up to date");
         git_commit_free(git_commit);
@@ -372,9 +353,7 @@ void commit_list_fetch_local(commit_list_t *commit_list, const char *path)
             break;
         }
 
-        if (old_last_commit_id &&
-            strcmp(old_last_commit_id,
-                   git_oid_tostr_s(git_commit_id(parent))) == 0)
+        if (old_last_commit_id && strcmp(old_last_commit_id, git_oid_tostr_s(git_commit_id(parent))) == 0)
         {
             puts("Last commit found");
             break;
@@ -401,8 +380,7 @@ void commit_list_fetch_local(commit_list_t *commit_list, const char *path)
             parent_tree = NULL;
         }
 
-        error = git_diff_tree_to_tree(&diff, repository, parent_tree,
-                                      commit_tree, NULL);
+        error = git_diff_tree_to_tree(&diff, repository, parent_tree, commit_tree, NULL);
         if (error < 0)
         {
             printf("6 %d\n", error);
@@ -414,8 +392,7 @@ void commit_list_fetch_local(commit_list_t *commit_list, const char *path)
         commit = commit_create();
         commit->commit_id = strdup(git_oid_tostr_s(git_commit_id(git_commit)));
         commit->timestamp = git_commit_time(git_commit);
-        error = git_diff_foreach(diff, each_file_cb, each_binary_cb,
-                                 each_hunk_cb, each_line_cb, commit);
+        error = git_diff_foreach(diff, each_file_cb, each_binary_cb, each_hunk_cb, each_line_cb, commit);
         if (error < 0)
         {
             printf("7 %d\n", error);
