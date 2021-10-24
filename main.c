eccbb222707093d1a5c2688d03ba76252d5448e1
Balázs János Varga <vargab95@gmail.com>
2021-10-24 17:51:45 +0200

    Add verbose cmd line option & call analysis

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "analysis.h"
 #include "commit_list.h"
 #include "object_tree.h"
 
@@ -32,7 +33,14 @@ int main(int argc, char **argv)
         puts("Cannot continue because commit list could not be loaded");
     }
 
+    m_list_t *analysis_libs = m_list_create();
+    m_com_sized_data_t tmp;
+    tmp.data = "./analyses/libgitalyze_analysis.so";
+    tmp.size = strlen(tmp.data);
+    m_list_append_to_end_set(analysis_libs, &tmp);
+
     build_object_tree(object_tree, commit_list);
+    execute_analyzes(object_tree, analysis_libs);
 
     commit_list_destroy(&commit_list);
     destroy_object_tree(&object_tree);
@@ -46,8 +54,7 @@ m_args_t *get_args(int argc, char **argv)
     m_args_add_entry(args, (m_args_entry_t){.id = ARG_REPOSITORY_PATH,
                                             .short_switch = "-p",
                                             .long_switch = "--path",
-                                            .environment_variable =
-                                                "GITALYZE_REPOSITORY_PATH",
+                                            .environment_variable = "GITALYZE_REPOSITORY_PATH",
                                             .flags = {.required = 0},
                                             .expected_type = ARG_TYPE_STRING,
                                             .preference = ARG_PREFER_SHORT});
@@ -55,40 +62,34 @@ m_args_t *get_args(int argc, char **argv)
     m_args_add_entry(args, (m_args_entry_t){.id = ARG_REPOSITORY_URL,
                                             .short_switch = "-u",
                                             .long_switch = "--url",
-                                            .environment_variable =
-                                                "GITALYZE_REPOSITORY_URL",
+                                            .environment_variable = "GITALYZE_REPOSITORY_URL",
                                             .flags = {.required = 0},
                                             .expected_type = ARG_TYPE_STRING,
                                             .preference = ARG_PREFER_SHORT});
 
-    m_args_add_entry(args,
-                     (m_args_entry_t){.id = ARG_COMMIT_LIST_PATH,
-                                      .short_switch = "-c",
-                                      .long_switch = "--commit-list-cache",
-                                      .environment_variable =
-                                          "GITALYZE_COMMIT_LIST_CACHE_PATH",
-                                      .flags = {.required = 0},
-                                      .expected_type = ARG_TYPE_STRING,
-                                      .preference = ARG_PREFER_SHORT});
+    m_args_add_entry(args, (m_args_entry_t){.id = ARG_COMMIT_LIST_PATH,
+                                            .short_switch = "-c",
+                                            .long_switch = "--commit-list-cache",
+                                            .environment_variable = "GITALYZE_COMMIT_LIST_CACHE_PATH",
+                                            .flags = {.required = 0},
+                                            .expected_type = ARG_TYPE_STRING,
+                                            .preference = ARG_PREFER_SHORT});
 
-    m_args_add_entry(args,
-                     (m_args_entry_t){.id = ARG_OBJECT_TREE_PATH,
-                                      .short_switch = "-o",
-                                      .long_switch = "--object-tree-cache",
-                                      .environment_variable =
-                                          "GITALYZE_OBJECT_TREE_CACHE_PATH",
-                                      .flags = {.required = 0},
-                                      .expected_type = ARG_TYPE_STRING,
-                                      .preference = ARG_PREFER_SHORT});
+    m_args_add_entry(args, (m_args_entry_t){.id = ARG_OBJECT_TREE_PATH,
+                                            .short_switch = "-o",
+                                            .long_switch = "--object-tree-cache",
+                                            .environment_variable = "GITALYZE_OBJECT_TREE_CACHE_PATH",
+                                            .flags = {.required = 0},
+                                            .expected_type = ARG_TYPE_STRING,
+                                            .preference = ARG_PREFER_SHORT});
 
-    m_args_add_entry(
-        args, (m_args_entry_t){.id = ARG_VERBOSE,
-                               .short_switch = "-v",
-                               .long_switch = "--verbose",
-                               .environment_variable = "GITALYZE_VERBOSE",
-                               .flags = {.required = 0, .no_value = 1},
-                               .expected_type = ARG_TYPE_STRING,
-                               .preference = ARG_PREFER_SHORT});
+    m_args_add_entry(args, (m_args_entry_t){.id = ARG_VERBOSE,
+                                            .short_switch = "-v",
+                                            .long_switch = "--verbose",
+                                            .environment_variable = "GITALYZE_VERBOSE",
+                                            .flags = {.required = 0, .no_value = 1},
+                                            .expected_type = ARG_TYPE_STRING,
+                                            .preference = ARG_PREFER_SHORT});
 
     m_args_parse(args, argc, argv);
 
@@ -100,8 +101,7 @@ int process_commit_list(commit_list_t *commit_list, m_args_t *args)
     m_args_entry_t *arg_entry;
     int result_code = 0;
 
-    if ((arg_entry = m_args_get(args, ARG_COMMIT_LIST_PATH)) &&
-        arg_entry->flags.present)
+    if ((arg_entry = m_args_get(args, ARG_COMMIT_LIST_PATH)) && arg_entry->flags.present)
     {
         FILE *fp = fopen(arg_entry->value.string_val, "rb");
         if (fp != NULL)
@@ -112,8 +112,7 @@ int process_commit_list(commit_list_t *commit_list, m_args_t *args)
         }
     }
 
-    if ((arg_entry = m_args_get(args, ARG_REPOSITORY_PATH)) &&
-        arg_entry->flags.present)
+    if ((arg_entry = m_args_get(args, ARG_REPOSITORY_PATH)) && arg_entry->flags.present)
     {
         commit_list_fetch_local(commit_list, arg_entry->value.string_val);
         result_code = 2;
@@ -124,8 +123,7 @@ int process_commit_list(commit_list_t *commit_list, m_args_t *args)
         commit_list_print(commit_list);
     }
 
-    if ((arg_entry = m_args_get(args, ARG_COMMIT_LIST_PATH)) &&
-        arg_entry->flags.present)
+    if ((arg_entry = m_args_get(args, ARG_COMMIT_LIST_PATH)) && arg_entry->flags.present)
     {
         FILE *fp = fopen(arg_entry->value.string_val, "wb");
         if (fp != NULL)
